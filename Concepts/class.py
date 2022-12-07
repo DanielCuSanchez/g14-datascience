ab03584d7ace597a4a3c5bdc11fa98b956c51bbf
DanielCu <danielcu@alternet.com.mx>
2022-12-06 20:11:59 -0600

    Challenge 1

--- Concepts/class.py
+++ Concepts/class.py
@@ -7,11 +7,20 @@ class Car:
     self.model = model
     self.year = year
     self.brand = brand
+# Forma 1
+  def __str__(self):
+    # example = self.model + "+" + self.year
+    return f"{self.model} + {self.year} + {self.brand}" # `${Variable} texto ${variable} `
 
 
 tesla = Car("S", 2022, "Tesla")
 
-print(tesla)
+# Forma 2
+print(tesla.model,tesla.year,tesla.brand, sep="-")
+print(tesla.year,tesla.brand,end =' - ')
+print(tesla.model)
+
+print("Example: ", tesla)
 print(type(tesla))
 
 
